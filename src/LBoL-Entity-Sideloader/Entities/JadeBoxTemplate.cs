41f078dcd12b92262e407238efdf1e99a21ba3e0
Neoshrimp <shadow638@protonmail.com>
2023-09-02 23:02:28 +0300

    bump

---
+++ src/LBoL-Entity-Sideloader/Entities/JadeBoxTemplate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LBoLEntitySideloader.Entities
+{
+    public abstract class JadeBoxTemplate : EntityDefinition
+    {
+        public override Type ConfigType()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Type EntityType()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public override Type TemplateType()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
