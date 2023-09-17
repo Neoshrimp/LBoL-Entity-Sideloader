62c18438927e3a384980c2852962739c92f866e9
IntoxicatedKid <58926732+IntoxicatedKid@users.noreply.github.com>
2023-09-17 22:17:25 +0900

    Update GameFixes.cs

--- src/LBoL-Entity-Sideloader/GameFixes.cs
+++ src/LBoL-Entity-Sideloader/GameFixes.cs
@@ -3,6 +3,7 @@ using LBoL.Base;
 using LBoL.ConfigData;
 using LBoL.Core.Battle.BattleActions;
 using LBoL.Core.Cards;
+using LBoL.Core.Units;
 using LBoL.Core;
 using System;
 using System.Collections.Generic;
@@ -377,10 +378,74 @@ namespace LBoLEntitySideloader
 
 
             }
-        
-        
 
 
+
+
+        }
+        [HarmonyPatch]
+        class AllowDuplicateExhibits1_Patch
+        {
+            static IEnumerable<MethodBase> TargetMethods()
+            {
+                yield return ExtraAccess.InnerMoveNext(typeof(GameRunController), nameof(GameRunController.GainExhibitRunner));
+            }
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+            {
+                CodeInstruction prevprevCi = null;
+                CodeInstruction prevCi = null;
+                foreach (var ci in instructions)
+                {
+                    if (prevprevCi != null && prevCi != null && ci.opcode == OpCodes.Call && prevCi.opcode == OpCodes.Call && prevprevCi.opcode == OpCodes.Callvirt)
+                    {
+                        yield return new CodeInstruction(OpCodes.Pop);
+                    }
+                    else if (prevprevCi != null && prevCi != null && ci.opcode == OpCodes.Ret && prevCi.opcode == OpCodes.Ldc_I4_0 && prevprevCi.opcode == OpCodes.Call)
+                    {
+                        yield return new CodeInstruction(OpCodes.Pop);
+                    }
+                    else if (prevprevCi != null && prevCi != null && ci.opcode == OpCodes.Newobj && prevCi.opcode == OpCodes.Ldstr && prevprevCi.opcode == OpCodes.Ldstr)
+                    {
+                        yield return new CodeInstruction(OpCodes.Pop);
+                    }
+                    else if (prevprevCi != null && prevCi != null && ci.opcode == OpCodes.Throw && prevCi.opcode == OpCodes.Newobj && prevprevCi.opcode == OpCodes.Ldstr)
+                    {
+                        yield return new CodeInstruction(OpCodes.Pop);
+                    }
+                    else
+                    {
+                        yield return ci;
+                    }
+                    prevprevCi = prevCi;
+                    prevCi = ci;
+                }
+            }
+        }
+        [HarmonyPatch(typeof(PlayerUnit), nameof(PlayerUnit.AddExhibit))]
+        class AllowDuplicateExhibits2_Patch
+        {
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                CodeInstruction prevprevCi = null;
+                CodeInstruction prevCi = null;
+                foreach (var ci in instructions)
+                {
+                    if (prevprevCi != null && prevCi != null && ci.opcode == OpCodes.Throw && prevCi.opcode == OpCodes.Newobj && prevprevCi.opcode == OpCodes.Ldstr)
+                    {
+                        yield return new CodeInstruction(OpCodes.Pop);
+                    }
+                    else if (prevprevCi != null && prevCi != null && ci.opcode == OpCodes.Newobj && prevCi.opcode == OpCodes.Ldstr && prevprevCi.opcode == OpCodes.Ldstr)
+                    {
+                        yield return new CodeInstruction(OpCodes.Pop);
+                    }
+                    else
+                    {
+                        yield return ci;
+                    }
+                    prevprevCi = prevCi;
+                    prevCi = ci;
+                }
+            }
         }
 
 
