670afe17e0c05a674816fac2673305178da0f12c
Neoshrimp <shadow638@protonmail.com>
2023-09-17 16:29:16 +0300

    merge point

---
+++ src/LBoL-Entity-Sideloader/Entities/StageTemplate.cs
@@ -0,0 +1,19 @@
+﻿using LBoL.ConfigData;
+using LBoL.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LBoLEntitySideloader.Entities
+{
+    public abstract class StageTemplate : EntityDefinition
+    {
+        public override Type ConfigType() => typeof(StageConfig);
+
+        public override Type EntityType() => typeof(Stage);
+
+
+        public override Type TemplateType() => typeof(StageTemplate);
+
+    }
+}
