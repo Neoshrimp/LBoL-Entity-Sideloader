4aecd689fff5ddcdd2976e4d6e62fa0136fbfbbc
cyaneko <contact@cyaneko.moe>
2024-12-13 01:34:43 +0100

    add StartGamePanel patches to unlock all difficulty buttons

--- src/LBoL-Entity-Sideloader/PluginInfo.cs
+++ src/LBoL-Entity-Sideloader/PluginInfo.cs
@@ -9,7 +9,7 @@ namespace LBoLEntitySideloader
     {
         public const string GUID = "neo.lbol.frameworks.entitySideloader";
         public const string description = "Entity Sideloader";
-        public const string version = "0.9.7601";
+        public const string version = "0.9.7602";
 
         public static readonly Harmony harmony = new Harmony(GUID);
     }
---
+++ src/LBoL-Entity-Sideloader/UIAdditions/StartGameUI/Patches.cs
@@ -0,0 +1,67 @@
+﻿using HarmonyLib;
+using LBoL.Core;
+using LBoL.Presentation.UI.ExtraWidgets;
+using LBoL.Presentation.UI.Panels;
+using UnityEngine;
+
+namespace LBoLEntitySideloader.UIAdditions.StartGameUI
+{
+    [HarmonyPatch(typeof(StartGamePanel))]
+    class StartGameUI_Patches
+    {
+        [HarmonyPatch(nameof(StartGamePanel.RefreshConfirm)), HarmonyPostfix]
+        static void UnlockCharacter(StartGamePanel __instance)
+        {
+            StartGamePanel p = __instance;
+
+            if (p._playerIndex >= 0 && p._playerIndex <= p._players.Length &&
+                null != p._players[p._playerIndex].Config.UnlockLevel)
+            {
+                p.characterHintRoot.gameObject.SetActive(false);
+                p.characterHint.text = "";
+                p.characterConfirmButton.interactable = true;
+            }
+        }
+
+        [HarmonyPatch(nameof(StartGamePanel.RefreshDifficultyConfirm)), HarmonyPostfix]
+        static void UnlockDifficulty(StartGamePanel __instance)
+        {
+            StartGamePanel p = __instance;
+
+            if (!p.gameModeSwitch.IsOn)
+            {
+                p.difficultyGroups[p._difficultyIndex].SetLocked(false);
+                p._isDifficultyLock = false;
+                p.difficultyConfirmButton.interactable = true;
+                p._confirmTooltip.SetDirect("");
+            }
+        }
+
+        [HarmonyPatch(nameof(StartGamePanel.SetPuzzleStatus)), HarmonyPostfix]
+        static void UnlockRequests(StartGamePanel __instance)
+        {
+            StartGamePanel p = __instance;
+
+            for (int i = 0; i < PuzzleFlags.AllPuzzleFlags.Count; ++i)
+            {
+                foreach (PuzzleToggleWidget ptw in p._puzzleToggles.Values)
+                {
+                    ptw.IsLock = false;
+                }
+            }
+
+            p.puzzleSelectAllGroup.SetActive(true);
+        }
+
+        [HarmonyPatch(nameof(StartGamePanel.SetJadeBoxStatus)), HarmonyPostfix]
+        static void UnlockJadeboxes(StartGamePanel __instance)
+        {
+            StartGamePanel p = __instance;
+
+            p.jadeBoxButton.interactable = true;
+            p._jadeBoxTooltip.enabled = false;
+            p.jadeBoxLockImage.gameObject.SetActive(false);
+            p.jadeBoxText.color = Color.white;
+        }
+    }
+}
